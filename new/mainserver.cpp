6ef0f633fd63bc94ea2410ee5f83d2f711fa827c
ewsnybchan7 <50603204+ewsnybchan7@users.noreply.github.com>
2019-12-13 04:58:00 +0900

    add thread
    add thread & list file

--- new/mainserver.cpp
+++ new/mainserver.cpp
@@ -1,6 +1,8 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <netinet/in.h>
+#include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,10 +15,56 @@
 #define MAX_BUF 100
 #define NAME_LEN 20
 #define COMMAND 5
+#define MAX_USER_NUM 5
 
-int tcp_listen(int host, int port, int backlog) {
-    int sd;
+typedef struct _user {
+    int userSock;
+    int userIdx;
+    int servSock;
+    char nick[NAME_LEN];
+} User;
+
+User list[MAX_USER_NUM];
+
+int sd;
+
+int useridx = 0;
+int userSocks[MAX_USER_NUM];
+pthread_mutex_t usermutex;
+
+void *tcp_listen(void *port);
+void *mainThread(void *us);
+void sigExit(int signum);
+
+int main(int argc, char *argv[]) {
+    pthread_t ptr; //
+    int port;
+
+    if (argc != 2) {
+        printf("usage: %s <port>\n", argv[0]);
+    }
+    port = atoi(argv[1]);
+
+    signal(SIGINT, sigExit);
+
+    pthread_mutex_init(&usermutex, NULL);
+
+    pthread_create(&ptr, NULL, tcp_listen, &port);
+    pthread_detach(ptr);
+
+    while (1) {
+        pause();
+    }
+
+    return 0;
+}
+
+void *tcp_listen(void *port) {
+    User user;
+    int servPort = *(int *)port;
+
     struct sockaddr_in servaddr;
+    int sd;
 
     sd = socket(AF_INET, SOCK_STREAM, 0);
     if (sd == -1) {
@@ -26,39 +74,88 @@ int tcp_listen(int host, int port, int backlog) {
 
     bzero((char *)&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(host);
-    servaddr.sin_port = htons(port);
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(servPort);
 
     if (bind(sd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
         perror("bind fail");
         exit(1);
     }
-    listen(sd, backlog);
-    return sd;
+    if (listen(sd, 5) == -1) {
+        perror("listen fail");
+        close(sd);
+    }
+
+    struct sockaddr_in client = {0};
+    int clen = sizeof(client);
+    pthread_t ptr;
+
+    printf("server open\n");
+    while ((user.userSock =
+                accept(sd, (struct sockaddr *)&client, (socklen_t *)&clen))) {
+        if (user.userSock == -1) {
+            perror("accept fail");
+            return 0;
+        }
+
+        pthread_mutex_lock(&usermutex);
+
+        user.userIdx = useridx;
+        user.servSock = sd;
+        userSocks[useridx] = user.userSock;
+        list[useridx] = user;
+        pthread_create(&ptr, NULL, mainThread, &user);
+        pthread_detach(ptr);
+        useridx++;
+        pthread_mutex_unlock(&usermutex);
+    }
+
+    return 0;
 }
 
-int main(int argc, char *argv[]) {
-    struct sockaddr_in server, client;
+void *mainThread(void *us) {
+    User user = *(User *)us;
     struct stat obj;
-    int servsock, clisock;
+    char name[NAME_LEN] = "";
+    char *userdir;
     char buf[MAX_BUF], command[COMMAND], fileName[NAME_LEN];
-    int k, i, size, len, c;
+    char lsbuf[MAX_BUF], filelist[MAX_BUF];
+    int k, i, size, c, dir;
     int filehandle;
 
-    servsock = tcp_listen(INADDR_ANY, atoi(argv[1]), 5);
+    recv(user.userSock, name, NAME_LEN, 0);
+    printf("%s Connect\n", name);
+    strcpy(user.nick, name);
+    userdir = (char *)malloc(strlen(name) + 2);
+    sprintf(userdir, "./%s", name);
 
-    len = sizeof(client);
-    clisock = accept(servsock, (struct sockaddr *)&client, (socklen_t *)&len);
+    dir = mkdir(userdir, 0777);
+    if (dir == -1) {
+        printf("%s is original user\n", user.nick);
+    }
+    dir = mkdir("./res", 0777);
+
+    sprintf(filelist, "./res/filelist_%s.txt", name);
+    sprintf(lsbuf, "touch %s ", filelist);
+    system(lsbuf);
+    chmod(filelist, 0666);
+    system("touch ./res/log.txt");
+    chmod("./res/log.txt", 0666);
+    chdir(userdir);
+    sprintf(filelist, "../res/filelist_%s.txt", name);
+    sprintf(lsbuf, "ls > %s", filelist);
+
     while (1) {
-        recv(clisock, buf, MAX_BUF, 0);
+        recv(user.userSock, buf, MAX_BUF, 0);
         sscanf(buf, "%s", command);
         if (!strcmp(command, "ls")) {
-            system("ls > temps.txt");
-            stat("temps.txt", &obj);
+            system(lsbuf);
+            stat(filelist, &obj);
             size = obj.st_size;
-            send(clisock, &size, sizeof(int), 0);
-            filehandle = open("temps.txt", O_RDONLY);
-            sendfile(clisock, filehandle, NULL, size);
+            send(user.userSock, &size, sizeof(int), 0);
+            filehandle = open(filelist, O_RDONLY);
+            sendfile(user.userSock, filehandle, NULL, size);
+
         } else if (!strcmp(command, "get")) {
             sscanf(buf, "%s%s", fileName, fileName);
             stat(fileName, &obj);
@@ -66,14 +163,14 @@ int main(int argc, char *argv[]) {
             size = obj.st_size;
             if (filehandle == -1)
                 size = 0;
-            send(clisock, &size, sizeof(int), 0);
+            send(user.userSock, &size, sizeof(int), 0);
             if (size)
-                sendfile(clisock, filehandle, NULL, size);
+                sendfile(user.userSock, filehandle, NULL, size);
         } else if (!strcmp(command, "put")) {
             int c = 0, len;
             char *f;
             sscanf(buf + strlen(command), "%s", fileName);
-            recv(clisock, &size, sizeof(int), 0);
+            recv(user.userSock, &size, sizeof(int), 0);
 
             while (1) {
                 filehandle = open(fileName, O_CREAT | O_EXCL | O_WRONLY, 0666);
@@ -83,31 +180,29 @@ int main(int argc, char *argv[]) {
                     break;
             }
             f = (char *)malloc(size);
-            recv(clisock, f, size, 0);
+            recv(user.userSock, f, size, 0);
             c = write(filehandle, f, size);
             close(filehandle);
-            send(clisock, &c, sizeof(int), 0);
-        } else if (!strcmp(command, "pwd")) {
-            system("pwd>temp.xt");
-            i = 0;
-            FILE *f = fopen("temp.txt", "r");
-            while (!feof(f))
-                buf[i++] = fgetc(f);
-            buf[i - 1] = '\0';
-            fclose(f);
-            send(clisock, buf, MAX_BUF, 0);
+            send(user.userSock, &c, sizeof(int), 0);
         } else if (!strcmp(command, "cd")) {
             if (chdir(buf + 3) == 0)
                 c = 1;
             else
                 c = 0;
-            send(clisock, &c, sizeof(int), 0);
-        } else if (!strcmp(command, "bye") || !strcmp(command, "quit")) {
-            printf("FTP server quitting..\n");
+            send(user.userSock, &c, sizeof(int), 0);
+        } else if (!strcmp(command, "quit")) {
+            printf("%s Quit..\n", user.nick);
             i = 1;
-            send(clisock, &i, sizeof(int), 0);
-            exit(0);
+            send(user.userSock, &i, sizeof(int), 0);
+            break;
         }
     }
+    free(userdir);
     return 0;
+}
+
+void sigExit(int signum) {
+    printf("close Server\n");
+    close(sd);
+    exit(0);
 }
