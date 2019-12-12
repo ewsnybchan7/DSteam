f42773c0735f7e9c8a20a37e1b983b546e594d5f
ewsnybchan7 <50603204+ewsnybchan7@users.noreply.github.com>
2019-12-13 00:26:54 +0900

    new client & server
    new client & server

--- mainserver.cpp
+++ mainserver.cpp
@@ -1,315 +1,113 @@
 #include <arpa/inet.h>
-#include <dirent.h>
 #include <fcntl.h>
-#include <netdb.h>
 #include <netinet/in.h>
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
+#include <sys/sendfile.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <time.h>
 #include <unistd.h>
 
-#define MAX_USER_NUM 5
-#define MAX_NICK_LEN 30
-#define MAX_MESSAGE_LEN 1024
+#define MAX_LINE 511
+#define MAX_BUF 100
+#define NAME_LEN 20
+#define COMMAND 5
 
-typedef struct _user {
-    int usd;
-    int unum;
-    int rsd;
-    char unick[MAX_NICK_LEN];
-} User;
+int tcp_listen(int host, int port, int backlog) {
+    int sd;
+    struct sockaddr_in servaddr;
 
-typedef struct _room {
-    int rsd;
-    int usd[5];
-    int ucnt;
-} Room;
+    sd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sd == -1) {
+        perror("socket fail");
+        exit(1);
+    }
 
-User list[5];
-Room rlist[5];
+    bzero((char *)&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(host);
+    servaddr.sin_port = htons(port);
 
-int sd;
-int rsd;
-int rcnt = 0;
-int usernum = 0;
-int rusernum = 0;
-int SERV_PORT;
-pthread_mutex_t usermutex, roommutex;
-
-void sigExit(int signum);
-int serverSetting(char *ip, int port);
-void *joinChat(void *user);
-void *deliveryMessage(void *user);
-void *notice(void *user);
-void whisper(User user, char *rbuf);
-void clientExit(User user);
-void *mainThread(void *room);
-void fileIO(User user, char *rbuf);
+    if (bind(sd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        perror("bind fail");
+        exit(1);
+    }
+    listen(sd, backlog);
+    return sd;
+}
 
 int main(int argc, char *argv[]) {
-    int room, i;
+    struct sockaddr_in server, client;
+    struct stat obj;
+    int servsock, clisock;
+    char buf[MAX_BUF], command[COMMAND], fileName[NAME_LEN];
+    int k, i, size, len, c;
+    int filehandle;
 
-    if (argc != 2) {
-        printf("usage: %s <port>\n", argv[0]);
-        exit(0);
-    }
+    servsock = tcp_listen(INADDR_ANY, atoi(argv[1]), 5);
 
-    SERV_PORT = atoi(argv[1]);
-
-    signal(SIGINT, sigExit);
-
-    pthread_mutex_init(&usermutex, NULL);
-    pthread_mutex_init(&roommutex, NULL);
-
-    printf("Input Room Number: ");
-    scanf("%d", &room);
-    printf("error before!\n");
-    getchar();
-
-    pthread_t thread[256];
-
-    for (i = 0; i < room; i++) {
-        pthread_create(&thread[i], NULL, mainThread, &room);
-        pthread_detach(thread[i]);
-    }
-    printf("after\n");
-    while (1)
-        pause();
-
-    return 0;
-}
-
-void *mainThread(void *room) {
-    pthread_mutex_lock(&roommutex);
-    User user;
-    Room rm;
-
-    int ssd;
-    int port = SERV_PORT + rcnt;
-    rm.ucnt = rcnt;
-
-    ssd = socket(AF_INET, SOCK_STREAM, 0);
-
-    struct sockaddr_in servaddr = {0};
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(port);
-    servaddr.sin_addr.s_addr = inet_addr("10.10.21.233");
-
-    if (bind(ssd, (struct sockaddr *)&servaddr, sizeof(servaddr))) {
-        perror("bind error!");
-        close(ssd);
-    }
-
-    if (listen(ssd, 5) == -1) {
-        perror("listen error");
-        close(ssd);
-    }
-
-    printf("port %d\n", port);
-
-    rcnt++;
-
-    pthread_mutex_unlock(&roommutex);
-
-    struct sockaddr_in cliaddr = {0};
-    int clen = sizeof(cliaddr);
-    pthread_t ptr;
-
-    while ((user.usd =
-                accept(ssd, (struct sockaddr *)&cliaddr, (socklen_t *)&clen))) {
-        if (user.usd == -1) {
-            perror("accept error");
-            return 0;
-        }
-
-        pthread_mutex_lock(&usermutex);
-
-        user.unum = usernum;
-
-        user.rsd = ssd;
-        rm.rsd = ssd;
-        rm.usd[rm.ucnt] == user.usd;
-
-        rlist[rm.ucnt] = rm;
-        pthread_create(&ptr, NULL, joinChat, &user);
-        pthread_detach(ptr);
-
-        usernum++;
-        rm.ucnt++;
-
-        pthread_mutex_unlock(&usermutex);
-    }
-
-    return 0;
-}
-
-void *joinChat(void *user) {
-    User us = *(User *)user;
-    char nick[MAX_NICK_LEN] = "";
-    pthread_t ptr[2];
-
-    send(us.usd, "Welcome Catting Program.\nInput your nickname.\n", 100, 0);
-    recv(us.usd, nick, sizeof(nick), 0);
-    nick[strlen(nick) - 1] = '\0';
-    printf("%s Connect\n", nick);
-
-    strcpy(us.unick, nick);
-
-    list[us.unum] = us;
-
-    pthread_create(&ptr[0], NULL, deliveryMessage, &us);
-    pthread_create(&ptr[1], NULL, notice, &us);
-    pthread_join(ptr[0], NULL);
-    pthread_join(ptr[1], NULL);
-}
-
-void *deliveryMessage(void *user) {
-    User us = *(User *)user;
-    int i;
-    char *wmsg;
-    char rbuf[MAX_MESSAGE_LEN];
-    char sbuf[MAX_MESSAGE_LEN];
-
-    while (recv(us.usd, rbuf, sizeof(rbuf), 0) > 0) {
-        if (!strncmp(rbuf, "/w", 2)) {
-            whisper(us, rbuf);
-            continue;
-        } else if (!strncmp(rbuf, "/f", 2)) {
-            fileIO(us, rbuf);
-            continue;
-        } else {
-            sprintf(sbuf, "%s : %s", us.unick, rbuf);
-            for (i = 0; i < usernum; i++) {
-                if (list[i].usd == us.usd)
-                    continue;
-                if (list[i].rsd == us.rsd)
-                    send(list[i].usd, sbuf, sizeof(sbuf), 0);
-            }
-        }
-
-        memset(rbuf, 0, sizeof(rbuf));
-        memset(sbuf, 0, sizeof(sbuf));
-    }
-}
-
-void fileIO(User user, char *rbuf) {
-    int fd;
-    char *fio;
-    char sbuf[MAX_MESSAGE_LEN];
-
-    fio = strtok(rbuf, " ");
-    fd = open(fio, O_RDONLY, 0644);
-
-    while (read(fd, rbuf, sizeof(rbuf)) > 0) {
-        if (send(user.usd, rbuf, sizeof(rbuf), 0) == -1) {
-            perror("send");
-            exit(1);
-        }
-    }
-}
-
-void clientExit(User user) {
-    int i, j;
-    char sbuf[MAX_MESSAGE_LEN];
-
-    sprintf(sbuf, "%s exit.\n", user.unick);
-
-    for (i = 0; i < usernum; i++) {
-        if (list[i].usd == user.usd) {
-            if (list[j].usd == user.usd)
-                continue;
-            if (list[j].rsd == user.rsd)
-                send(list[j].usd, sbuf, sizeof(sbuf), 0);
-        }
-        break;
-    }
-
-    pthread_mutex_lock(&usermutex);
-    for (j = i; j < usernum - 1; j++) {
-        list[j] = list[j + 1];
-    }
-    usernum--;
-    pthread_mutex_unlock(&usermutex);
-}
-
-void whisper(User user, char *rbuf) {
-    int i;
-    char *whisp;
-    char sbuf[MAX_MESSAGE_LEN];
-
-    strtok(rbuf, " ");
-    whisp = strtok(NULL, " ");
-
-    for (i = 0; i < usernum; i++) {
-        if (!strcmp(whisp, list[i].unick)) {
-            whisp = strtok(NULL, " ");
-            sprintf(sbuf, "%s whisper: %s", user.unick, whisp);
-            if (list[i].rsd == user.rsd)
-                send(list[i].usd, sbuf, strlen(sbuf), 0);
-            break;
-        }
-    }
-
-    if (i == usernum)
-        send(user.usd, "no user.\n", 30, 0);
-}
-
-void *notice(void *user) {
-    int i;
-    User us = *(User *)user;
-    char sbuf[MAX_MESSAGE_LEN];
-    char inbuf[MAX_MESSAGE_LEN];
-
-    sprintf(sbuf, "%s come.\n", us.unick);
-    for (i = 0; i < usernum; i++) {
-        if (list[i].rsd == us.rsd)
-            send(list[i].usd, sbuf, strlen(sbuf), 0);
-    }
-
+    len = sizeof(client);
+    clisock = accept(servsock, (struct sockaddr *)&client, (socklen_t *)&len);
     while (1) {
-        memset(sbuf, 0, sizeof(sbuf));
-        memset(inbuf, 0, sizeof(inbuf));
-        fgets(inbuf, sizeof(inbuf), stdin);
-        sprintf(sbuf, "[Notice]: %s\n", inbuf);
-        for (i = 0; i < usernum; i++)
-            send(list[i].usd, sbuf, strlen(sbuf), 0);
+        recv(clisock, buf, MAX_BUF, 0);
+        sscanf(buf, "%s", command);
+        if (!strcmp(command, "ls")) {
+            system("ls > temps.txt");
+            stat("temps.txt", &obj);
+            size = obj.st_size;
+            send(clisock, &size, sizeof(int), 0);
+            filehandle = open("temps.txt", O_RDONLY);
+            sendfile(clisock, filehandle, NULL, size);
+        } else if (!strcmp(command, "get")) {
+            sscanf(buf, "%s%s", fileName, fileName);
+            stat(fileName, &obj);
+            filehandle = open(fileName, O_RDONLY);
+            size = obj.st_size;
+            if (filehandle == -1)
+                size = 0;
+            send(clisock, &size, sizeof(int), 0);
+            if (size)
+                sendfile(clisock, filehandle, NULL, size);
+        } else if (!strcmp(command, "put")) {
+            int c = 0, len;
+            char *f;
+            sscanf(buf + strlen(command), "%s", fileName);
+            recv(clisock, &size, sizeof(int), 0);
+
+            while (1) {
+                filehandle = open(fileName, O_CREAT | O_EXCL | O_WRONLY, 0666);
+                if (filehandle == -1)
+                    sprintf(fileName + strlen(fileName), "_1");
+                else
+                    break;
+            }
+            f = (char *)malloc(size);
+            recv(clisock, f, size, 0);
+            c = write(filehandle, f, size);
+            close(filehandle);
+            send(clisock, &c, sizeof(int), 0);
+        } else if (!strcmp(command, "pwd")) {
+            system("pwd>temp.xt");
+            i = 0;
+            FILE *f = fopen("temp.txt", "r");
+            while (!feof(f))
+                buf[i++] = fgetc(f);
+            buf[i - 1] = '\0';
+            fclose(f);
+            send(clisock, buf, MAX_BUF, 0);
+        } else if (!strcmp(command, "cd")) {
+            if (chdir(buf + 3) == 0)
+                c = 1;
+            else
+                c = 0;
+            send(clisock, &c, sizeof(int), 0);
+        } else if (!strcmp(command, "bye") || !strcmp(command, "quit")) {
+            printf("FTP server quitting..\n");
+            i = 1;
+            send(clisock, &i, sizeof(int), 0);
+            exit(0);
+        }
     }
-}
-
-int serverSetting(char *ip, int port) {
-    int ssd;
-    ssd = socket(AF_INET, SOCK_STREAM, 0);
-
-    struct sockaddr_in servaddr = {0};
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(port);
-    servaddr.sin_addr.s_addr = inet_addr(ip);
-
-    if (bind(ssd, (struct sockaddr *)&servaddr, sizeof(servaddr))) {
-        perror("bind error");
-        close(ssd);
-        return -1;
-    }
-
-    if (listen(ssd, 5) == -1) {
-        perror("listen error");
-        close(ssd);
-        return -1;
-    }
-    return ssd;
-}
-
-void sigExit(int signum) {
-    printf("close server\n");
-    close(rsd);
-    close(sd);
-    exit(0);
+    return 0;
 }
